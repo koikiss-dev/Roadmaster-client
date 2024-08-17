313a9f1d9c71574d3b0a54d662e137284fcde136
yako <duronj08@gmail.com>
2024-08-16 22:50:05 -0600

    agregue mas rutas

---
+++ resources/views/sales.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('subtitle', 'Ventas')
+
+@section('content_body')
+    <header class="">
+        <h1>Ventas</h1>
+    </header>
+
+
+
+@stop
+
+@push('js')
+    <script></script>
+@endpush
