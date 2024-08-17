76dc1f3749b17eab8b7d18cf897aabc542fd1000
yako <duronj08@gmail.com>
2024-08-16 22:31:29 -0600

    vehiculos

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get("/vehiculos", [VehicleController::class, 'index']);
-
-Route::get('/vehiculos/{id}', [VehicleController::class, 'show']);
